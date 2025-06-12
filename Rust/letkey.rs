69d490950535ca8c1fcab4e3a0b207f6bd2401cd
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-06-12 19:02:33 +0530

    letkey.rs

---
+++ Rust/letkey.rs
@@ -0,0 +1,11 @@
+// let keyword example
+// This program demonstrates the use of the `let` keyword in Rust to declare variables. 
+
+fn main()
+{
+    let comstr= "Shakal";
+    let f = 4.5;
+    let g_bool = true;
+    let ichar = 'a';
+    println!("Name is :{}\n Float is :{}\n Boolean is :{}\n Char is :{}", comstr, f, g_bool, ichar);
+}
