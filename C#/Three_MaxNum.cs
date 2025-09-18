970e86e77bd682e0edf78cb3d12870fac7b55888
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-09-18 17:46:26 +0530

    Three_MaxNum.cs

---
+++ C#/Three_MaxNum.cs
@@ -0,0 +1,24 @@
+namespace Three_MaxNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Number: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Third Number: ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+            if (n1 == n2 && n2 == n3)
+            {
+                Console.WriteLine("All Numbers Are Equal");
+            }
+            else
+            {
+                int max = Math.Max(n1, Math.Max(n2, n3));
+                Console.WriteLine("Maximum Number: " + max);
+            }
+        }
+    }
+}
